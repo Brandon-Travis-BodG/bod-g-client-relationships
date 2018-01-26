a3fe5ec2ed63f22a1389bf58ee1be3e08a583bc3
brandongossen <brandongossen@yahoo.com>
2018-01-26 00:56:29 -0600

    Modify the Next and Previous Function
    -Refactor the former 'next' and 'previous' functions to anonymous functions as the function is only ever called in one place and it makes the code more self-contained.

--- src/main/resources/static/js/registration-pagination.js
+++ src/main/resources/static/js/registration-pagination.js
@@ -1,3 +1,7 @@
+(function () {
+
+$( document ).ready(function() {
+
 var listElement = $(".page");
 var perPage = 6;
 var numItems = listElement.length;
@@ -5,40 +9,38 @@ var numPages = Math.ceil(numItems/perPage);
 console.log(numItems);
 console.log(numPages);
 
-$('.pager').attr("currentPage", 0);
-//.attr is creating and setting an attribute called currentPage with a value of 0.
+$(".pager").attr("currentPage",0);
+//.attr is creating and setting an attribute called currentpage with a value of 0.
 
 
-$('.pager').addClass('active');
+$(".pager").addClass("active");
 
-listElement.css('display', 'none');
-listElement.slice(0, perPage).css('display', 'block');
+listElement.css("display", "none");
+listElement.slice(0, perPage).css("display", "block");
 
-$("#previous").click(previous);
-function previous(){
-    var currPage = $('.pager').attr("currentPage");
+$("#previous").click(function(){
+    var currPage = $(".pager").attr("currentpage");
     var goToPage = parseInt(currPage) - 1;
     if(goToPage >= 0){
         goTo(goToPage);
-    }console.log(currPage);
+    }
+    console.log(currPage);
     console.log(goToPage);
-}
-//
-$("#next").click(next);
-function next(){
-    var currPage = $('.pager').attr("currentPage");
+});
+
+$("#next").click(function(){
+    var currPage = $(".pager").attr("currentpage");
     var goToPage = parseInt(currPage) + 1;
     if(goToPage < numPages){
         goTo(goToPage);
     }
     console.log(currPage);
     console.log(goToPage);
-}
-
+});
 //.attr or .data either sets or gets the value in the html
 //page resets the currentPage to whatever is passed in depending on if next or previous is passed in.
 
-//Coming from the DOM it comes in as a string so must convert it as a number so it doesn't concatenate.
+//Coming from the DOM it comes in as a string so must convert it as a number so it doesn"t concatenate.
 
 //.attr is getting the value of the current page attribute and parseInt turns the value of the current page from a string to a number.
 
@@ -49,9 +51,10 @@ function goTo(page){
     var endOn = startAt + perPage;
     //page  starts on index 6 and go to index 11 cause of the slice.
 
-
-    listElement.css('display','none');
-    listElement.slice(startAt, endOn).css('display','block');
-    $('.pager').attr("currentPage", page);
+    listElement.css("display","none");
+    listElement.slice(startAt, endOn).css("display","block");
+    $(".pager").attr("currentPage",page);
 }
+});
+})();
 //page resets the currentPage to whatever is passed in depending on if next or previous is passed in.
