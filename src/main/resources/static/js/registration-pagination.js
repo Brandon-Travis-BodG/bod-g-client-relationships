d77b5a959c99f7871229d48e3461a681a7c9060c
brandongossen <brandongossen@yahoo.com>
2018-01-25 23:56:43 -0600

    Pagination for Registration Page Functionality Restructure 1
    -Create a variable that target the form entries $(".page").
    
    -Create variables that instruct how many entries can be on a page and how many pages the registration will consist of.

--- src/main/resources/static/js/registration-pagination.js
+++ src/main/resources/static/js/registration-pagination.js
@@ -1,14 +1,6 @@
-$('.form-group').pagination({
-    items: 10,
-    itemOnPage: 6,
-    currentPage: 1,
-    cssStyle: '',
-    prevText: '<span aria-hidden="true">&laquo;</span>',
-    nextText: '<span aria-hidden="true">&raquo;</span>',
-    onInit: function goToPage (pageNumber) {
-        currentPage = pageNumber;
-    },
-    onPageClick: $(".pagination-page").click(function(){
-        goToPage($(this).text());
-    })
-});
+var listElement = $(".page");
+var perPage = 6;
+var numItems = listElement.length;
+var numPages = Math.ceil(numItems/perPage);
+console.log(numItems);
+console.log(numPages);
