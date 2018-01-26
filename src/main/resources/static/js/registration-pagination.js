a6650400c995e69c81c918f1195fecb42992237f
brandongossen <brandongossen@yahoo.com>
2018-01-26 00:05:08 -0600

    Pagination for Registration Page Functionality Restructure 2
    -Have the pager class create an attribute called currentPage that will mark what page # the user is on in the registration form.
    
    -Create a function called goTo that takes a page as an argument and displays at what form entry each page of the registration form will start and end on.
    
    -Use css to determine when certain entries should be displayed and vice versa.

--- src/main/resources/static/js/registration-pagination.js
+++ src/main/resources/static/js/registration-pagination.js
@@ -4,3 +4,28 @@ var numItems = listElement.length;
 var numPages = Math.ceil(numItems/perPage);
 console.log(numItems);
 console.log(numPages);
+
+$('.pager').attr("currentPage",0);
+//.attr is creating and setting an attribute called currentpage with a value of 0.
+
+
+$('.pager').addClass('active');
+
+listElement.css('display', 'none');
+listElement.slice(0, perPage).css('display', 'block');
+
+
+function goTo(page){
+    var startAt = page * perPage;
+    //page 1 would be index 0 * 6, and there is a slice so only 0 to 5 will be take.
+
+    var endOn = startAt + perPage;
+    //page  starts on index 6 and go to index 11 cause of the slice.
+
+
+    listElement.css('display','none');
+    listElement.slice(startAt, endOn).css('display','block');
+    $('.pager').attr("currentPage",page);
+}
+//.attr or .data either sets or gets the value in the html.
+//page resets the currentPage to whatever is passed in depending on if next or previous is passed in.
