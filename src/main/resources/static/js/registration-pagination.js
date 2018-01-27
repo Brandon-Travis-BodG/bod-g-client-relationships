514dfb518ef5950d261e094d44742fe77351bf8b
brandongossen <brandongossen@yahoo.com>
2018-01-26 23:36:50 -0600

    Pagination edit
    -Only display the next button on the first page of the registration form, by using the .hide() method with the 'previous' attribute.
    
    -Only display the previous button on the second page of the registration form, by using the .hide() method with the 'next' attribute.

--- src/main/resources/static/js/registration-pagination.js
+++ src/main/resources/static/js/registration-pagination.js
@@ -1,42 +1,48 @@
 (function () {
 
-$( document ).ready(function() {
+    $( document ).ready(function() {
 
-var listElement = $(".page");
-var perPage = 6;
-var numItems = listElement.length;
-var numPages = Math.ceil(numItems/perPage);
-console.log(numItems);
-console.log(numPages);
+        var listElement = $(".page");
+        var perPage = 6;
+        var numItems = listElement.length;
+        var numPages = Math.ceil(numItems/perPage);
+        console.log(numItems);
+        console.log(numPages);
 
-$(".pager").attr("currentPage", 0);
+        $(".pager").attr("currentPage", 0);
 //.attr is creating and setting an attribute called currentpage with a value of 0.
 
 
-$(".pager").addClass("active");
+        $(".pager").addClass("active");
 
-listElement.css("display", "none");
-listElement.slice(0, perPage).css("display", "block");
+        listElement.css("display", "none");
+        listElement.slice(0, perPage).css("display", "block");
 
-$("#previous").click(function(){
-    var currPage = $(".pager").attr("currentPage");
-    var goToPage = parseInt(currPage) - 1;
-    if(goToPage >= 0){
-        goTo(goToPage);
-    }
-    console.log(currPage);
-    console.log(goToPage);
-});
+        $("#previous").hide();
 
-$("#next").click(function(){
-    var currPage = $(".pager").attr("currentPage");
-    var goToPage = parseInt(currPage) + 1;
-    if(goToPage < numPages){
-        goTo(goToPage);
-    }
-    console.log(currPage);
-    console.log(goToPage);
-});
+        $("#previous").click(function(){
+            var currPage = $(".pager").attr("currentPage");
+            var goToPage = parseInt(currPage) - 1;
+            if(goToPage >= 0){
+                goTo(goToPage);
+            }
+            $(this).hide();
+            $("#next").show();
+            console.log(currPage);
+            console.log(goToPage);
+        });
+
+        $("#next").click(function(){
+            var currPage = $(".pager").attr("currentPage");
+            var goToPage = parseInt(currPage) + 1;
+            if(goToPage < numPages){
+                goTo(goToPage);
+            }
+            $(this).hide();
+            $("#previous").show();
+            console.log(currPage);
+            console.log(goToPage);
+        });
 //.attr or .data either sets or gets the value in the html
 //page resets the currentPage to whatever is passed in depending on if next or previous is passed in.
 
@@ -44,17 +50,17 @@ $("#next").click(function(){
 
 //.attr is getting the value of the current page attribute and parseInt turns the value of the current page from a string to a number.
 
-function goTo(page){
-    var startAt = page * perPage;
-    //page 1 would be index 0 * 6, and there is a slice so only 0 to 5 will be take.
+        function goTo(page){
+            var startAt = page * perPage;
+            //page 1 would be index 0 * 6, and there is a slice so only 0 to 5 will be take.
 
-    var endOn = startAt + perPage;
-    //page  starts on index 6 and go to index 11 cause of the slice.
+            var endOn = startAt + perPage;
+            //page  starts on index 6 and go to index 11 cause of the slice.
 
-    listElement.css("display","none");
-    listElement.slice(startAt, endOn).css("display","block");
-    $(".pager").attr("currentPage", page);
-}
-});
+            listElement.css("display","none");
+            listElement.slice(startAt, endOn).css("display","block");
+            $(".pager").attr("currentPage", page);
+        }
+    });
 })();
 //page resets the currentPage to whatever is passed in depending on if next or previous is passed in.
