21f8cb6b4f345205c48880f9dcba26bb72681663
brandongossen <brandongossen@yahoo.com>
2018-01-30 23:21:22 -0600

    Register Validation Model Page
    -Add Spring annotations in the User Model in order to verify that the information provided by the user is not blank.
    
    -Add Spring annotations in the User Model in order to verify that the Phone Number provided by the user is formatted with dashes and has to be 10 digits long.
    
    -Add Spring annotations in the User Model in order to verify that age, height, and weight can only be numbers.
    
    -Have these validations generate a set of error messages in case the input provided by the user is invalid.

--- src/main/java/com/brandongossen/bodg/clientmanager/models/User.java
+++ src/main/java/com/brandongossen/bodg/clientmanager/models/User.java
@@ -1,8 +1,15 @@
 package com.brandongossen.bodg.clientmanager.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 import static javax.persistence.CascadeType.ALL;
 
@@ -15,35 +22,51 @@ public class User {
     private long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "First name can not be blank!")
     private String firstName;
 
     @Column(nullable = false)
+    @NotBlank(message = "Last name can not be blank!")
     private String lastName;
 
     @Column(nullable = false)
+    @NotBlank(message = "Email can not be blank!")
+    @Email(message = "Email must have an @ symbol!")
     private String email;
 
     @Column(nullable = false)
+    @NotBlank(message = "Phone number can not be blank!")
+    @Size(min=10, message = "Phone number must be 10 digits String!")
+    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{4}",
+            message = "Invalid phone number")
     private String phoneNumber;
 
     @Column(nullable = false)
+    @NotBlank(message = "Username can not be blank!")
     private String username;
 
     @Column(nullable = false)
+    @NotBlank(message = "Password can not be blank!")
     private String password;
 
     @Column(nullable = false)
+    @NumberFormat(style= NumberFormat.Style.NUMBER)
+//    @NotBlank(message = "Age can not be blank!")
     private short age;
 
     @Column(nullable = false)
     private Gender gender;
-    //gives things to choose from
+    //enum gives things to choose from
     //char allow a single character
 
     @Column(nullable = false)
+    @NumberFormat(style= NumberFormat.Style.NUMBER)
+//    @NotBlank(message = "Height can not be blank!")
     private short height;
 
     @Column(nullable = false)
+    @NumberFormat(style= NumberFormat.Style.NUMBER)
+//    @NotBlank(message = "Weight can not be blank!")
     private double weight;
 
 
@@ -71,7 +94,7 @@ public class User {
         weight = copy.weight;
     }
 
-    public User(Long id, String firstName, String lastName, String email, String phoneNumber, String username, String password, short age, Gender gender, short height, double weight) {
+    public User(long id, String firstName, String lastName, String email, String phoneNumber, String username, String password, short age, Gender gender, short height, double weight) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
