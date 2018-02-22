85d5ce1f6675d49a7b2fc62572cc0275738cc7c4
brandongossen <brandongossen@yahoo.com>
2018-02-22 00:33:30 -0600

    Workout Manager API Connection
    -Chain promises: Use the response object's (resolved from the fetch method's promise) .json method that returns another promise that returns a JSON object in order to manipulate the results of the fetch request.
    
    -Load data from the Workout Manager API using a get request and add the data to the homepage.
    
    -Create an anonymous forEach function that will display the exercise name and exercise description as a list.
    
    -Connect the workout-manager.js file to the home.html file by referencing the div id.

--- src/main/resources/static/js/workout-manager.js
+++ src/main/resources/static/js/workout-manager.js
@@ -1,30 +1,28 @@
 (function () {
     "use strict";
-    // $(document).ready(function () {
-
-        // const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
-        const url = "https://wger.de/api/v2/workout/";
-        const options = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-            },
-            // body: JSON.stringify(blogPost),
-        };
-        const myPromise = fetch(url, options);
-            myPromise.then(() => console.log("resolved!"));
-            myPromise.catch(() => console.log("Rejected!"));
-        // const myPromise = fetch('https://api.github.com/users');
-        // myPromise.then(response => console.log(response));
-        // myPromise.catch(error => console.error(error));
-// });
+    const url = "https://wger.de/api/v2/exercise/";
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
+        }
+        // body: '{"key": "value"}'
+        // body: JSON.stringify("),
+    };
+    const myPromise = fetch(url, options);
+    myPromise
+        .then((response) => response.json())
+        //.json is a method that is returning another promise, which will now return a json object.
+        .then((response) => {
+            var html = "<ul>";
+            response.results.forEach(function (exercise) {
+                html += `<li> ${exercise.name}</li>`;
+                html += `<li> ${exercise.description}</li>`;
+            });
+            html += "</ul>";
+            document.getElementById('exercises').innerHTML = html;
+            // $('#exercises').html(html);
+        });
+    myPromise.catch(() => console.log("Rejected!"));
 })();
-
-// # In the request header
-// "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-//
-//
-// # Example with curl
-//     curl -X GET https://wger.de/api/v2/workout/ \
-//     -H 'Authorization: Token 1234567890abcde...'
