f604fa66db92b02d37ef72a2eb36c26cc672364c
brandongossen <brandongossen@yahoo.com>
2018-03-01 18:09:18 -0600

    Workout Manager API Match Exercise Image
    -Create a new view to store the WGER API exercise data called exercises.html.
    
    -Make the Workout Regimine header on the Home page an anchor that links to an Exercise page.
    
    -Create a method called exercise in the HomeController that returns exercises.html

--- src/main/java/com/brandongossen/bodg/clientmanager/controllers/HomeController.java
+++ src/main/java/com/brandongossen/bodg/clientmanager/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String homePage() {
             return "home";
     }
+
+@GetMapping("/exercises")
+    public String exercise() {
+        return "exercises";
+    }
 }
--- src/main/resources/static/js/workout-manager.js
+++ src/main/resources/static/js/workout-manager.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    // const url = "https://wger.de/api/v2/exerciseimage/?limit=400";
+    const url = "https://wger.de/api/v2/exerciseimage/?limit=400";
     const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
     const options = {
         method: "GET",
@@ -18,9 +18,9 @@
         .then((response) => {
             let html = "<ul>";
             response.results.forEach(function (exercise) {
-                // html += `<li> ${exercise.name}</li>`;
-                // html += `<li> ${exercise.description}</li>`;
-                html += `<li><img id="image" src="${exercise.image}"</img></li>`;
+                html += `<li> ${exercise.name}</li>`;
+                html += `<li> ${exercise.description}</li>`;
+                // html += `<li><img id="image" src="${exercise.image}"</img></li>`;
             });
             html += "</ul>";
             document.getElementById('exercises').innerHTML = html;
@@ -63,44 +63,43 @@
 //
 //
 //
-// //     const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
-// //     const options = {
-// //         method: "GET",
-// //         headers: {
-// //             "Content-Type": "application/json",
-// //             "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-// //         }
-// //         // body: '{"key": "value"}'
-// //         // body: JSON.stringify("),
-// //     };
-// //     const myPromise = fetch(url, options);
-// //     myPromise
-// //         .then((response) => response.json())
-// //         //.json is a method that is returning another promise, which will now return a json object.
-// //         .then((exercises) => {
-// //
-// //
-// //             let html = "<ul>";
-// //             let urls = [];
-// //             exercises.results.forEach(function (exercise) {
-// //                 // urls.push("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id);
-// //                 //html += `<li> ${exercise.name}</li>`;
-// //                 //html += `<li> ${exercise.description}</li>`;
-// //
-// //                 fetch("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id)
-// //                     .then(response => response.json())
-// //                     .then(image => {
-// //                         html += `<li><img id="image" src="${exercise.image}"</img></li>`;
-// //                     })
-// //                 ;
-// //
-// //             });
-// //             html += "</ul>";
-// //             document.getElementById('exercises').innerHTML = html;
-// //             // $('#exercises').html(html);
-// //         });
-// //     myPromise.catch(() => console.log("Rejected!"));
-// // })();
+//     const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
+//     const options = {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
+//         }
+//         // body: '{"key": "value"}'
+//         // body: JSON.stringify("),
+//     };
+//     const myPromise = fetch(url, options);
+//     myPromise
+//         .then((response) => response.json())
+//         //.json is a method that is returning another promise, which will now return a json object.
+//         .then((exercises) => {
+//
+//
+//             let html = "<ul>";
+//             let urls = [];
+//             exercises.results.forEach(function (exercise) {
+//                 // urls.push("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id);
+//                 //html += `<li> ${exercise.name}</li>`;
+//                 //html += `<li> ${exercise.description}</li>`;
+//                 fetch("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id)
+//                     .then(response => response.json())
+//                     .then(image => {
+//                         html += `<li><img id="image" src="${exercise.image}"</img></li>`;
+//                     })
+//                 ;
+//
+//             });
+//             html += "</ul>";
+//             document.getElementById('exercises').innerHTML = html;
+//             // $('#exercises').html(html);
+//         });
+//     myPromise.catch(() => console.log("Rejected!"));
+// })();
 // //
 // //
 // // // const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
