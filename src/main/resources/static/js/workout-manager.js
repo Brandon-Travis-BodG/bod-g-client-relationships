1172ce017b4ca926b9ef7c8db87b5fa4d91b6e3a
brandongossen <brandongossen@yahoo.com>
2018-03-20 17:27:27 -0500

    Body-Parts anatomical Text Alignment Edit
    -Create a javascript function  that gets query string values by making the category an object.
    
    -Refactor the if statement to replace the static category number with a statement that makes the exercise category the user clicks on equal to the number associated with the category in the body-parts.html.
    
    -Delete some of the commented code out for better appearance (Will continue to do this).

--- src/main/resources/static/js/workout-manager.js
+++ src/main/resources/static/js/workout-manager.js
@@ -1,6 +1,20 @@
 (function () {
     "use strict";
 
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    //changing the query string into an object
+
+    const category = getParameterByName('category');
+
     // "https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id
 
 
@@ -38,120 +52,50 @@
             // }];
 
             exercises.results.forEach(function (exercise) {
-                if (exercise.category == 8) {
-                    // return exercise.name && exercise.description
-                    // }
-                    // else (exercise.category !== 10)
-                    // {
-                    //     return;
-                    // }
-                    // }
-                    // urls.push("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id);
-                    // put each element inside obj
-                    html += `<h3 id="exerciseName"> ${exercise.name}</h3>`;
-                    html += `<li> ${exercise.description}</li>`;
-                    html += `<li id="exercise-${exercise.id}"></li>`;
-                    // var string;
-                    // for loop
-                    // ex[i].name, ex[i].description, ex[i].id
-                    // after loop --> string = <li>ex1.name</li>....
-                    fetch("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id)
-                    // put each element inside object in the correct exercise
-                    // obj.ex1.picture =
-                        .then(response => response.json())
-                        .then(images => {
-                            console.log(images);
-                            if (images.results.length === 0) return;
-                            // let imageHtml = `<li><img id="image" src="${image.results[0].image}"</img></li>`;
-                            // $(`#exercise-${exercise.id}`).html(imageHtml);
-                            let imageHtml = "<li>";
-                            images.results.forEach(function (image) {
-                                imageHtml += `<img id="image" src="${image.image}"</img></li>`;
-                            });
-                            imageHtml += "<li>";
-                            $(`#exercise-${exercise.id}`).html(imageHtml);
+                if (exercise.category === Number(category)) {
+                // return exercise.name && exercise.description
+                // }
+                // else (exercise.category !== 10)
+                // {
+                //     return;
+                // }
+                // }
+                // urls.push("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id);
+                // put each element inside obj
+                html += `<h3 id="exerciseName"> ${exercise.name}</h3>`;
+                html += `<li> ${exercise.description}</li>`;
+                html += `<li id="exercise-${exercise.id}"></li>`;
+                // var string;
+                // for loop
+                // ex[i].name, ex[i].description, ex[i].id
+                // after loop --> string = <li>ex1.name</li>....
+                fetch("https://wger.de/api/v2/exerciseimage/?exercise=" + exercise.id)
+                // put each element inside object in the correct exercise
+                // obj.ex1.picture =
+                    .then(response => response.json())
+                    .then(images => {
+                        console.log(images);
+                        if (images.results.length === 0) return;
+                        // let imageHtml = `<li><img id="image" src="${image.results[0].image}"</img></li>`;
+                        // $(`#exercise-${exercise.id}`).html(imageHtml);
+                        let imageHtml = "<li>";
+                        images.results.forEach(function (image) {
+                            imageHtml += `<img id="image" src="${image.image}"</img></li>`;
                         });
-                }
+                        imageHtml += "<li>";
+                        $(`#exercise-${exercise.id}`).html(imageHtml);
+                    });
+            }
             });
             html += "</ul>";
     // $("#arms").click(function (event) {
     //     event.preventDefault();
     //     $("#exercises").show(exercises.category == 8);
-        // exercise.category == 8;
+    //     // exercise.category == 8;
     // });
-            document.getElementById('arm-exercises').innerHTML = html;
+            document.getElementById('exercises').innerHTML = html;
             // $('#exercises').html(html);
         });
     myPromise.catch(() => console.log("Rejected!"));
 
 })();
-// (function () {
-//     "use strict";
-// const url = "https://wger.de/api/v2/exerciseimage/?limit=400";
-//     const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
-//     const options = {
-//         method: "GET",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-//         }
-//         // body: '{"key": "value"}'
-//         // body: JSON.stringify("),
-//     };
-//     const myPromise = fetch(url, options);
-//     myPromise
-//         .then((response) => response.json())
-//         //.json is a method that is returning another promise, which will now return a json object.
-//         .then((response) => {
-//             let html = "<ul>";
-//             response.results.forEach(function (exercise) {
-//                 html += `<li> ${exercise.name}</li>`;
-//                 html += `<li> ${exercise.description}</li>`;
-//                 // html += `<li><img id="image" src="${exercise.image}"</img></li>`;
-//             });
-//             html += "</ul>";
-//             document.getElementById('exercises').innerHTML = html;
-//             // $('#exercises').html(html);
-//             console.log(response);
-//         });
-//     myPromise.catch(() => console.log("Rejected!"));
-// })();
-
-
-// const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
-// const options = {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json",
-//         "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-//     },
-//     // body: '{"key": "value"}'
-//     // body: JSON.stringify("),
-// };
-// const myPromise = fetch(url, options);
-// myPromise
-//     .then((response) => response.json())
-//     //.json is a method that is returning another promise, which will now return a key
-//     .then((response) => console.log(response));
-// myPromise.catch(() => console.log("Rejected!"));
-
-
-// //
-// //
-// // // const url = "https://wger.de/api/v2/exercise/?language=2&limit=500";
-// // // const options = {
-// // //     method: "GET",
-// // //     headers: {
-// // //         "Content-Type": "application/json",
-// // //         "Authorization": "Token a68a4eaf22f78199a0abdeb073fd368b9d6ee74e"
-// // //     },
-// // //     // body: '{"key": "value"}'
-// // //     // body: JSON.stringify("),
-// // // };
-// // // const myPromise = fetch(url, options);
-// // // myPromise
-// // //     .then((response) => response.json())
-// // //     //.json is a method that is returning another promise, which will now return a key
-// // //     .then((response) => console.log(response));
-// // // myPromise.catch(() => console.log("Rejected!"));
-
